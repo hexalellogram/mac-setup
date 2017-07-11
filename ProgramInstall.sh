b0a94f6b4597be2e9c901ae22c5092cd93ac2a25
Bryce Tsuyuki <brycetsuyuki@icloud.com>
2017-07-10 23:59:22 -0700

    organize and prioritize

--- ProgramInstall.sh
+++ ProgramInstall.sh
@@ -5,7 +5,7 @@ read installAnswer
 if echo "$installAnswer" | grep -iq "^n" ;then
     exit
 else
-    echo You have chosen to proceed with program installation.
+    echo You have chosen to proceed with application installation.
 fi
 
 # Set Xcode path to Xcode-beta
@@ -15,7 +15,7 @@ xcode-select -s /Applications/Xcode-beta.app
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
-# Install brew command line tools
+# Install command line tools from brew
 brew install mas
 brew install wget
 brew install tree
@@ -25,36 +25,48 @@ brew install archey
 # Enable cask-versions
 brew tap caskroom/verisons
 
-# Install apps from cask
+# Install apps from brew cask
+
+# Core/Essential Apps
 brew cask install 1password-beta
+brew cask install google-chrome-beta
+brew cask install insync
+brew cask install bettertouchtool
+brew cask install steam # high priority so I can begin downloading games
+echo We just installed Steam so please log into Steam and begin downloading your games now, thanks.
+brew cask install carbon-copy-cloner
+brew cask install spotify
+brew cask install microsoft-office
+brew cask install vmware-fusion
+
+
+# Menubar Apps
+brew cask install dropbox
+brew cask install google-drive
+brew cask install nordvpn
+brew cask install semulov
+brew cask install spotify-notifications
+brew cask install yujitach-menumeters
+
+# Objective-See Security Apps
+brew cask install blockblock
+brew cask install oversight
+brew cask install ransomwhere
+
+# Code/Computer Science
+brew cask install gitkraken
+brew cask install java
+brew cask install eclipse-java
+brew cask install visual-studio-code
+brew cask install powershell
+
+# Other Apps/Utilities
 brew cask install adobe-acrobat-reader
 brew cask install android-file-transfer
 brew cask install appcleaner
-brew cask install bettertouchtool
-brew cask install carbon-copy-cloner
-brew cask install dropbox
-brew cask install gitkraken
-brew cask install google-chrome-beta
-brew cask install google-drive
-brew cask install insync
-brew cask install microsoft-office
-brew cask install nordvpn
-brew cask install oversight
-brew cask install ransomwhere
-brew cask install semulov
-brew cask install spotify
-brew cask install spotify-notifications
-brew cask install steam
 brew cask install the-unarchiver
 brew cask install tuxera-ntfs
-brew cask install visual-studio-code
-brew cask install vmware-fusion
-brew cask install java
-brew cask install eclipse-java
-brew cask install yujitach-menumeters
-brew cask install blockblock
 brew cask install osxfuse
-brew cask install powershell
 
 # Install quick look generators
 brew cask install betterzipql 
@@ -77,4 +89,4 @@ mas install 409203825 # Numbers
 brew prune
 brew cleanup
 
-echo Core apps not installed and must be installed manually: Acrok Video Converter, Winclone, all Steam games
+echo Apps not installed that must be installed manually: Acrok Video Converter, Winclone, all Steam games
