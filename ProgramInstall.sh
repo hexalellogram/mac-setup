dc1814de73a7ca8b41ef6e248a66e9a32f3c71cb
Vinay V <cool00geek@yahoo.com>
2017-07-12 22:31:43 -0700

    Follow shell guidelines and idiot proof a bit more

--- ProgramInstall.sh
+++ ProgramInstall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Do you want to proceed with installation of Homebrew and all programs, and is Xcode (or Xcode-beta) installed? (Y/n)"
-read installAnswer
-if echo "$installAnswer" | grep -iq "^n" ;then
-    exit
+echo "Do you want to proceed with installation of Homebrew and all programs, and is Xcode (or Xcode-beta) installed? (Y/n): "
+read -n installAnswer
+if echo "$installAnswer" | grep -iq "^n"; then
+    exit 1
 else
-    echo You have chosen to proceed with application installation.
+    echo "You have chosen to proceed with application installation."
+fi
+
+echo -n "Making sure Xcode-beta is installed..."
+if [ -d /Applications/Xcode-beta.app ]; then
+    echo " Pass!"
+else
+    echo " Fail! Exiting..."
+    exit 1
 fi
 
 # Set Xcode path to Xcode-beta
@@ -16,6 +24,7 @@ xcode-select -s /Applications/Xcode-beta.app
 brew update
 
 # Install command line tools from brew
+echo "Installing command line apps"
 brew install mas
 brew install wget
 brew install tree
@@ -28,12 +37,14 @@ brew tap caskroom/verisons
 # Install apps from brew cask
 
 # Core/Essential Apps
+echo "Installing Core/Essential Apps"
+brew cask install steam # high priority so I can begin downloading games
+sh /Applications/Steam.app/Contents/MacOS/steam.sh >/dev/null 2>&1 &
+echo "We just installed Steam so please log into Steam and begin downloading your games now, thanks."
 brew cask install 1password-beta
 brew cask install google-chrome-beta
 brew cask install insync
 brew cask install bettertouchtool
-brew cask install steam # high priority so I can begin downloading games
-echo We just installed Steam so please log into Steam and begin downloading your games now, thanks.
 brew cask install carbon-copy-cloner
 brew cask install spotify
 brew cask install microsoft-office
@@ -41,6 +52,7 @@ brew cask install vmware-fusion
 
 
 # Menubar Apps
+echo "Installing Menubar Apps"
 brew cask install dropbox
 brew cask install google-drive
 brew cask install onedrive
@@ -50,11 +62,13 @@ brew cask install spotify-notifications
 brew cask install yujitach-menumeters
 
 # Objective-See Security Apps
+echo "Installing Security Apps"
 brew cask install blockblock
 brew cask install oversight
 brew cask install ransomwhere
 
 # Code/Computer Science
+echo "Installing Code/Computer Science Apps"
 brew cask install gitkraken
 brew cask install java
 brew cask install eclipse-java
@@ -62,6 +76,7 @@ brew cask install visual-studio-code
 brew cask install powershell
 
 # Other Apps/Utilities
+echo "Installing other Apps/Utilities"
 brew cask install adobe-acrobat-reader
 brew cask install android-file-transfer
 brew cask install appcleaner
@@ -70,6 +85,7 @@ brew cask install tuxera-ntfs
 brew cask install osxfuse
 
 # Install quick look generators
+echo "Installing quick look generators"
 brew cask install betterzipql 
 brew cask install qlcolorcode 
 brew cask install qlmarkdown 
@@ -80,13 +96,19 @@ brew tap buo/cask-upgrade
 brew cask install brew-cask-upgrade
 
 # Install apps from Mac App Store
+echo "Installing Mac App Store Apps"
 mas install 937984704 # Amphetamine
 mas install 409201541 # Pages
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
 
 # Cleanup
+echo "Cleaning up"
 brew prune
 brew cleanup
 
-echo Apps not installed that must be installed manually: Acrok Video Converter, Winclone, all Steam games
+echo "The following Apps must be installed manually:"
+echo "    Acrok Video Converter"
+echo "    Winclone"
+echo "    All Steam games"
+# Apps not installed that must be installed manually: Acrok Video Converter, Winclone, all Steam games"
