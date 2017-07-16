fa972af55b762688cf2e6fb6020cf913703a9738
Bryce Tsuyuki <brycetsuyuki@icloud.com>
2017-07-16 01:16:50 -0700

    add startup apps, misc additions

--- ProgramInstall.sh
+++ ProgramInstall.sh
@@ -52,22 +52,29 @@ brew cask install carbon-copy-cloner
 brew cask install spotify
 brew cask install microsoft-office
 brew cask install vmware-fusion
+echo "Core/Essential Apps installed!"
 
 # Menubar Apps
 echo "Installing Menubar Apps"
 brew cask install dropbox
-brew cask install google-drive
+# brew cask install google-drive    # removed for now because google-drive has been replaced with google-backup-and-sync BUT 
+                                    # that is a client for personal accounts, I use google-drive for a G Suite account so I 
+                                    # am still waiting on Google to release Drive File Stream which is the new G Suite app
+                                    # which will replace Google Drive for G Suite accounts. Remember to change the Google 
+                                    # Drive login item below as well when Drive File Stream arrives
 brew cask install onedrive
 brew cask install nordvpn
 brew cask install semulov
 brew cask install spotify-notifications
 brew cask install yujitach-menumeters
+echo "Menubar Apps Installed!"
 
 # Objective-See Security Apps
-echo "Installing Security Apps"
+echo "Installing Objective-See Security Apps"
 brew cask install blockblock
 brew cask install oversight
 brew cask install ransomwhere
+echo "Objective-See Security Apps Installed"
 
 # Code/Computer Science
 echo "Installing Code/Computer Science Apps"
@@ -76,6 +83,7 @@ brew cask install java
 brew cask install eclipse-java
 brew cask install visual-studio-code
 brew cask install powershell
+echo "Code/Computer Science Apps Installed!"
 
 # Other Apps/Utilities
 echo "Installing Other Apps/Utilities"
@@ -86,13 +94,15 @@ brew cask install the-unarchiver
 brew cask install tuxera-ntfs
 brew cask install osxfuse
 brew cask install android-platform-tools
+echo "Other Apps/Utilities Installed!"
 
 # Install quick look generators
-echo "Installing Finder Quick Look generators"
+echo "Installing Finder Quick Look Generators"
 brew cask install betterzipql 
 brew cask install qlcolorcode 
 brew cask install qlmarkdown 
 brew cask install qlstephen
+echo "Finder Quick Look Generators Installed!"
 
 # install brew-cask-upgrade
 brew tap buo/cask-upgrade
@@ -104,11 +114,33 @@ mas install 937984704 # Amphetamine
 mas install 409201541 # Pages
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
+echo "Mac App Store Apps Installed!"
 
 # Cleanup
 echo "Cleaning up"
 brew prune
 brew cleanup
+
+
+echo "Setting Startup Applications"
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Reminders.app", hidden:false, name:"Reminders"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Mail.app", hidden:false, name:"Mail"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Messages.app", hidden:false, name:"Messages"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spotify.app", hidden:false, name:"Spotify"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Steam.app", hidden:false, name:"Steam"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/BetterTouchTool.app", hidden:false, name:"BetterTouchTool"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spotify Notifications.app", hidden:false, name:"Spotify Notifications"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/NordVPN.app", hidden:false, name:"NordVPN"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/OverSight.app/Contents/Library/LoginItems/OverSight Helper.app", hidden:false, name:"OverSight Helper"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"~/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent.app", hidden:false, name:"Android File Transfer Agent"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Insync.app", hidden:false, name:"Insync"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Google Drive.app", hidden:false, name:"Google Drive"}' # remove when Google Drive is replaced with Drive File Stream
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"~/Library/PreferencePanes/MenuMeters.prefPane/Contents/Resources/MenuMetersApp.app", hidden:false, name:"MenuMetersApp"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Semulov.app", hidden:false, name:"Semulov"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Carbon Copy Cloner.app/Contents/Library/LoginItems/CCC User Agent.app", hidden:false, name:"CCC User Agent"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Dropbox.app", hidden:false, name:"Dropbox"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Amphetamine.app", hidden:false, name:"Amphetamine"}'
+echo "Startup Applications Set!"
 
 echo "Installation of all programs complete!"
 echo "The following apps must be installed manually:"
