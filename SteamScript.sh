60ea292e7f6fd0b7e0ec6c27cdb0ce671b1afab1
Bryce Tsuyuki <brycetsuyuki@icloud.com>
2017-07-17 16:09:59 -0700

    quit steamcmd

--- SteamScript.sh
+++ SteamScript.sh
@@ -9,15 +9,15 @@ echo "You will need to enter your Steam Guard code for the first time soon so ha
 echo "This should be a one-time only occurrence since after this Steam will use cached credentials."
 
 echo "Installing Team Fortress 2"
-steamcmd +login $username $password +app_update 440 validate
+steamcmd +login $username $password +app_update 440 validate +quit
 
 echo "Installing Kerbal Space Program"
-steamcmd +login $username $password +app_update 220200 validate
+steamcmd +login $username $password +app_update 220200 validate +quit
 
 echo "Installing Planetary Annihilation"
-steamcmd +login $username $password +app_update 232250 validate
+steamcmd +login $username $password +app_update 232250 validate +quit
 
 echo "Installing Civilization V + DLC"
-steamcmd +login $username $password +app_update 8930 validate # DLC is included with this
+steamcmd +login $username $password +app_update 8930 validate  +quit # DLC is included with this
 
 echo "Steam Games Installed! You may now close this Terminal window"
