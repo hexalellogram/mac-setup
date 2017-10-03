3f8b681a6e66e1c0aac2a5959c7a408901daf5e2
Bryce Tsuyuki <btsuyuki1@gmail.com>
2017-10-02 17:30:53 -0700

    accept xcode license

--- ProgramInstall.sh
+++ ProgramInstall.sh
@@ -26,6 +26,8 @@ else
     exit 1
 fi
 
+sudo xcodebuild -license
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
